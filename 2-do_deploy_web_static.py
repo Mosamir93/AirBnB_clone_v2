4a8df19335986fc2e3d71f8ff8cdcaadb1fd854e
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-07 02:31:42 +0300

    task-2 new 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,46 @@
 #!/usr/bin/python3
-"""Compress web static package
 """
-from fabric.api import *
-from datetime import datetime
-from os import path
+Fabric script to deploy tgz archive
+fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
+    -i private-key -u user
+"""
+
+from os.path import exists
+from fabric.api import put, run, env
 
 env.hosts = ['52.87.230.55', '100.25.150.51']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
     """
-    Deploy web files to server
+    copies archive file from local to my webservers
     """
+
+    if not exists(archive_path):
+        return False
     try:
-        if not (path.exists(archive_path)):
-            return False
+        file_name = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, "/tmp/")
 
-        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}".format(file_name))
 
-        timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/\
-            releases/web_static_{}/'.format(timestamp))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name))
 
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-            /data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
 
-        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
+            .format(file_name, file_name))
 
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-            /data/web_static/releases/web_static_{}/'.format(
-                timestamp, timestamp))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
 
-        run('sudo rm -rf /data/web_static/releases/\
-            web_static_{}/web_static'
-            .format(timestamp))
+        run('rm -rf /data/web_static/current')
 
-        run('sudo rm -rf /data/web_static/current')
-
-        run('sudo ln -s /data/web_static/releases/\
-            web_static_{}/ /data/web_static/current'.format(timestamp))
+        run(('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current')
+            .format(file_name))
+        return True
     except Exception:
         return False
-
-    return True
