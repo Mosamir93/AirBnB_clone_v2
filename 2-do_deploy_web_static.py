a1b6a73ab60170e02b7129bdc10cf34ee7c8908b
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-05-07 03:02:56 +0300

    task-2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ fab -f 2-do_deploy_web_static.py do_deploy:archive_path=filepath
 """
 
 import os.path
-from fabric.api import put, run, env
+from fabric.api import *
 
 env.hosts = ['52.87.230.55', '100.25.150.51']
 
@@ -16,31 +16,21 @@ def do_deploy(archive_path):
     copies archive file from local to my webservers
     """
 
-    if not os.path.isfile(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
     try:
-        file_name = archive_path.split("/")[-1].split(".")[0]
-        full_path = "/data/web_static/releases/{}/".format(file_name)
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        full_path = "/data/web_static/releases/{}/".format(no_ext)
         symlink = "/data/web_static/current"
         put(archive_path, "/tmp/")
-
         run("mkdir -p {}".format(full_path))
-
-        run("tar -xzf /tmp/{}.tgz -C {}"
-            .format(file_name, full_path))
-
-        run("rm /tmp/{}.tgz".format(file_name))
-
-        run(("mv {}web_static/* {}")
-            .format(full_path, full_path))
-
-        run("rm -rf {}web_static"
-            .format(full_path))
-
-        run("rm -rf {}").format(symlink)
-
-        run(("ln -s {} {}")
-            .format(full_path, symlink))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, full_path))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(full_path, full_path))
+        run("rm -rf {}web_static".format(full_path))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(full_path, symlink))
         return True
     except Exception:
         return False
